a2d25ed0321f338133cdcdaef1b617c109a3aa09
zalyora <48105531+zalyora@users.noreply.github.com>
2021-03-19 13:54:06 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('#app').vide({
+        mp4: '../video/video.mp4',
+        webm: '../video/video.ogv',
+        ogv: '../video/webm',
+        poster: '../img/cover.jpg'
+    });
+});
